ad1ec2be61d5ea68575dd2ea7ccdb8ce9f44edd5
alex.zubov <alex.zubov@sintez.co.za>
2019-11-02 23:26:45 +0200

    In runserver.py added gevent

--- runserver.py
+++ runserver.py
@@ -5,9 +5,15 @@ This script runs the docker_monitor application using a development server.
 
 from os import environ
 from docker_monitor import app
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     app_host = environ.get('SERVER_HOST', '0.0.0.0')
     app_port = int(environ.get('SERVER_PORT', '5355'))
- 
-    app.run(app_host, app_port)
+    app_env  = environ.get('ENV', 'dev') # prod
+    
+    if 'prod' in app_env:         
+        http_server = WSGIServer((app_host, int(app_port)), app)
+        http_server.serve_forever()
+    elif 'dev' in app_env:
+        app.run(app_host, app_port)
