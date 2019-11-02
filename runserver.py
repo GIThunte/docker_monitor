bf50500d97dc295eb59fa59c603e017c01083a59
alex.zubov <alex.zubov@sintez.co.za>
2019-11-02 23:53:24 +0200

    In runserver.py renamed var

--- runserver.py
+++ runserver.py
@@ -7,7 +7,7 @@ from gevent.pywsgi import WSGIServer
 if __name__ == '__main__':
     app_host = environ.get('SERVER_HOST', '0.0.0.0')
     app_port = int(environ.get('SERVER_PORT', '5355'))
-    app_env  = environ.get('ENV', 'dev') # prod
+    app_env  = environ.get('SERVER_ENV', 'dev') # prod
     
     if 'prod' in app_env:         
         http_server = WSGIServer((app_host, int(app_port)), app)
