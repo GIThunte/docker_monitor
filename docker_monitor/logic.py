afb06c6634ef2c542586415f6e4bf1490470c13b
alex.zubov <alex.zubov@sintez.co.za>
2019-11-08 00:26:54 +0200

    Added add new hosts in db functionsl

--- docker_monitor/logic.py
+++ docker_monitor/logic.py
@@ -2,6 +2,7 @@ from docker_monitor import app
 from datetime import datetime, timedelta
 from threading import Thread
 from multiprocessing import Queue
+from pymongo import MongoClient
 import threading
 import docker
 
@@ -83,3 +84,50 @@ def run_threading_host_status(host_lists):
         out_status.append(get_threads.get())
 
     return out_status
+
+def get_mongo_client(host, port):
+    try:
+        return MongoClient(host, port)
+    except Exception as e:
+        logger(e)
+    
+def get_database(host, port, db_name):
+    try:
+        return get_mongo_client(host, port)[db_name]
+    except Exception as e:
+        logger(e)
+
+def get_collection(host, port, db_name, collection_name):
+    try:
+        return get_database(host, port, db_name)[collection_name]
+    except Exception as e:
+        logger(e)
+
+def get_hosts_for_sp(host, port, db_name, collection_name):
+    json_obj = {}
+    collection_obj = get_collection(host, port, db_name, collection_name).find()
+    for iter_hosts in collection_obj:
+        json_obj[iter_hosts['host_name']] = iter_hosts['addr']
+        
+    return json_obj
+
+def get_hosts_for_add_hl(host, port, db_name, collection_name):
+    return [x for x in get_collection(host, port, db_name, collection_name).find()]
+
+def insert_mongo_data(host, port, db_name, collection_name, insert_data):
+    try:
+        return get_collection(host, port, db_name, collection_name).insert_one(insert_data).inserted_id
+    except Exception as e:
+        logger(e)   
+
+def run_insert(host, port, db_name, collection_name, insert_data):
+    if insert_mongo_data(host, port, db_name, collection_name, insert_data):
+        return True
+    else:
+        return False
+
+def check_exits_host(host, port, db_name, collection_name, check_data):
+    if get_collection(host, port, db_name, collection_name).find_one(check_data):
+        return True
+    else:
+        return False
--- docker_monitor/views.py
+++ docker_monitor/views.py
@@ -9,11 +9,38 @@ from jinja2 import Template
 import docker
 import json
 
-host_lists = {'localhost': 'tcp://10.1.0.65:3325', 'web': 'tcp://10.1.0.65:3322'}
+ip      = '10.1.0.65'
+port    = 27017
+db      = 'host_lists'
+coll    = 'hosts'
+
+@app.route('/add_new_host', methods=['GET', 'POST'])
+def add_new_host():
+    if request.method == 'POST':
+        host_name = request.form['hostname']
+        host_addr = request.form['hostaddr']
+        
+        if host_addr and host_name:
+            try:
+                if logic.check_docker_status(docker.APIClient(base_url=host_addr), host_addr):
+                    logic.run_insert(ip, port, db, coll, {'host_name': host_name, 'addr': host_addr})
+                else:
+                    return render_template('add_host.html', error='invalid address')
+                       
+            except Exception as e:
+                    logic.logger(e)
+                    return render_template('add_host.html', error='invalid address')
+                    
+            return render_template('add_host.html')
+        else:
+            error = 'You must add host address and host name'
+            return render_template('add_host.html', error=error)
+    
+    return render_template('add_host.html')
 
 @app.route('/get_host_list')
 def get_host_list():
-    return json.dumps(logic.run_threading_host_status(host_lists))
+    return json.dumps(logic.run_threading_host_status(logic.get_hosts_for_sp(ip, port, db, coll)))
 
 @app.route('/')
 def dashboard():
